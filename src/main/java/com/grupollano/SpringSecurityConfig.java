e5d13c591d4b2640436f32234b523c84b9f4045a
oscar baquero <osbbarr1@gmail.com>
2022-05-03 04:45:16 -0500

    proyecto inicial

---
+++ src/main/java/com/grupollano/SpringSecurityConfig.java
@@ -0,0 +1,14 @@
+package com.grupollano;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.User.UserBuilder;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+public class SpringSecurityConfig {
+
+	
+}
---
+++ src/main/java/com/grupollano/controllers/LoginController.java
@@ -0,0 +1,20 @@
+package com.grupollano.controllers;
+
+import java.util.List;
+
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+
+@CrossOrigin
+@RestController
+@RequestMapping(value = "/app/v1")
+public class LoginController {
+	
+	
+	
+}
